2f2ed5b65ab5096219457692f0e9349aaa324a63
TijilM <tijilmalhotra@gmail.com>
2024-07-31 02:32:08 +0530

    features of news and portfolio added

--- pages/News.py
+++ pages/News.py
@@ -1,15 +1,18 @@
 import streamlit as st
 import requests
 
-# API key and URL for NewsAPI
-API_KEY = 'c843fc74ad7d44e28e00d37b802ce86f'
-API_URL = 'https://newsapi.org/v2/everything'
+# API key and URL for Financial Modeling Prep
+# API_KEY = 'IIox350AjA5uTmnil9oxoZkWkmLy8Urc'
+# API_URL = 'https://financialmodelingprep.com/api/v3/search-ticker?query=AA&limit=10&exchange=NASDAQ'
 
-def fetch_data(keyword):
+
+API_KEY = 'c843fc74ad7d44e28e00d37b802ce86f'
+API_URL = 'https://newsapi.org/v2/everything?q=tesla&from=2024-06-30&sortBy=publishedAt'
+
+def fetch_data(query):
     params = {
-        'q': keyword,
-        'sortBy': 'publishedAt',
-        'apiKey': API_KEY
+        'query': query,
+        'apikey': API_KEY
     }
     response = requests.get(API_URL, params=params)
     return response.json()
@@ -33,15 +36,14 @@ def display_articles(articles):
 def main():
     st.title('Financial News Carousel')
 
-    keyword = st.text_input('Enter keyword for news search', 'financial')
-    if keyword:
-        data = fetch_data(keyword)
+    query = 'financial'  # Modify this query based on your needs
+    data = fetch_data(query)
 
-        if data and 'articles' in data and data['articles']:
-            st.header('Latest Financial News')
-            display_articles(data['articles'])
-        else:
-            st.write("No data available or failed to fetch data.")
+    if data and 'articles' in data and data['articles']:
+        st.header('Latest Financial News')
+        display_articles(data['articles'])
+    else:
+        st.write("No data available or failed to fetch data.")
 
 if __name__ == "__main__":
     main()
